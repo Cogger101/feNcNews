74db10a92db952737cfc5195ecf6de51d580fb99
Cogger101 <seandcoughlan@hotmail.co.uk>
2024-06-18 21:27:06 +0100

    tidied up navbar and added icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,19 @@
+import React from "react";
 import { Link } from "react-router-dom"
-import Profile from "./Profile"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faNewspaper, faFolder, faUser, faPenNib } from "@fortawesome/free-solid-svg-icons";
+
+
 
 function NavBar() {
     return (
         <nav className="navbar">
-            <Link to="/articles" >Home</Link>
-            <Link> Topics</Link>
-            <Link to="/articles"> Articles</Link>
-            <Link> Authors</Link>
-            <Link> Profile</Link>
+
+            <Link className="navbar-btn" to="/articles" ><FontAwesomeIcon icon={faHouse} /> Home</Link>
+            <Link className="navbar-btn"><FontAwesomeIcon icon={faFolder} /> Topics</Link>
+            <Link className="navbar-btn" to="/articles"><FontAwesomeIcon icon={faNewspaper} /> Articles</Link>
+            <Link className="navbar-btn"><FontAwesomeIcon icon={faPenNib} /> Authors</Link>
+            <Link className="navbar-btn"><FontAwesomeIcon icon={faUser} /> Profile</Link>
         </nav>
     )
 }
