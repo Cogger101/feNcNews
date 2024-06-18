70c7987c64da55c5ed09682ca66a69a9b92cfc7a
Cogger101 <seandcoughlan@hotmail.co.uk>
2024-06-18 21:59:40 +0100

    styling comment cards and general positioning

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -3,37 +3,37 @@ import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import CommentCard from "./CommentCard"
 
-function Comments (){
+function Comments() {
     const [comments, setComments] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
-    const {article_id} = useParams()
+    const { article_id } = useParams()
 
 
-    useEffect(()=>{
-        getArticleComments(article_id).then((response)=>{
+    useEffect(() => {
+        getArticleComments(article_id).then((response) => {
             setLoading(false)
             setComments(response.data.comments)
-        }).catch((error)=>{
+        }).catch((error) => {
             setLoading(false)
             setError('Error fetching comments')
             console.log('Error fetching comment:', error)
         })
-    },[article_id])
+    }, [article_id])
 
-    if (loading){
+    if (loading) {
         return <p>Loading...</p>
     }
-    if (error){
+    if (error) {
         return <p>{error}</p>
     }
 
 
     return (
         <>
-        {comments.map(comment =>( 
-        <CommentCard key={comment.comment_id} comment={comment}/>
-        ))}
+            {comments.map(comment => (
+                <CommentCard key={comment.comment_id} comment={comment} />
+            ))}
         </>
     )
 }
